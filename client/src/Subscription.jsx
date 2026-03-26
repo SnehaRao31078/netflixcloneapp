2ecb0c5e2e31ca6986a501a59765ce5a046a3816
SnehaRao31078 <sneharao31078@gmail.com>
2026-03-26 19:57:55 +0530

    Added index changes

--- client/src/Subscription.jsx
+++ client/src/Subscription.jsx
@@ -1,5 +1,5 @@
 import "./sub.css";
-import { useNavigate,Link } from "react-router-dom";
+import { useNavigate} from "react-router-dom";
 import axios from "axios";
 import { useEffect } from "react";
     {
