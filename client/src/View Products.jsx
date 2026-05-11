e96d7b719a209dc090be7edb9131c4d642c42070
SnehaRao31078 <sneharao31078@gmail.com>
2026-05-11 19:05:20 +0530

    changes

--- client/src/View Products.jsx
+++ client/src/View Products.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+/*import { useEffect, useState } from "react";
 import axios from "axios";
 import "./view.css";
 import { Link } from "react-router-dom";
@@ -104,6 +104,186 @@ function ViewProducts() {
           ))}
         </tbody>
       </table>
+    </div>
+  );
+}
+
+export default ViewProducts;*/
+
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./view.css";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+
+function ViewProducts() {
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+
+    axios
+      .get(`${import.meta.env.VITE_API_URL}/products`)
+
+      .then((res) => {
+        setMovies(res.data);
+      })
+
+      .catch((err) => console.log(err));
+
+  }, []);
+
+  const handleDelete = (id) => {
+
+    axios
+      .delete(`${import.meta.env.VITE_API_URL}/products/${id}`)
+
+      .then(() => {
+
+        toast.success("Deleted successfully");
+
+        setMovies(
+          movies.filter(
+            (movie) => movie._id !== id
+          )
+        );
+      })
+
+      .catch((err) => console.log(err));
+  };
+
+  return (
+
+    <div className="view">
+
+      <h2>Movies List</h2>
+
+      <table border="1">
+
+        <thead>
+
+          <tr>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Language</th>
+            <th>Category</th>
+            <th>Image</th>
+            <th>Video</th>
+            <th>Plan</th>
+            <th colSpan={2}>Action</th>
+          </tr>
+
+        </thead>
+
+        <tbody>
+
+          {
+            movies.map((movie) => (
+
+              <tr key={movie._id}>
+
+                <td>{movie.title}</td>
+
+                <td>{movie.description}</td>
+
+                <td>{movie.language}</td>
+
+                <td>{movie.category}</td>
+
+                {/* IMAGE */}
+                <td>
+
+                  <img
+                    src={movie.file}
+                    width="100"
+                    alt={movie.title}
+                  />
+
+                </td>
+
+                {/* VIDEO */}
+                <td>
+
+                  {
+                    movie.video ? (
+
+                      <video
+                        width="220"
+                        height="120"
+                        muted
+                        loop
+                        autoPlay
+                        playsInline
+                        controls
+                        poster={movie.file}
+                      >
+
+                        <source
+                          src={movie.video}
+                          type="video/mp4"
+                        />
+
+                      </video>
+
+                    ) : movie.youtubeLink ? (
+
+                      <iframe
+                        width="220"
+                        height="120"
+                        src={movie.youtubeLink.replace(
+                          "watch?v=",
+                          "embed/"
+                        )}
+                        title="YouTube Video"
+                        allowFullScreen
+                      ></iframe>
+
+                    ) : (
+
+                      <p>No Video</p>
+
+                    )
+                  }
+
+                </td>
+
+                <td>{movie.plan}</td>
+
+                {/* EDIT */}
+                <td>
+
+                  <Link to={`/edit/${movie._id}`}>
+
+                    <button className="up">
+                      Edit
+                    </button>
+
+                  </Link>
+
+                </td>
+
+                {/* DELETE */}
+                <td>
+
+                  <button
+                    className="del"
+                    onClick={() =>
+                      handleDelete(movie._id)
+                    }
+                  >
+                    Delete
+                  </button>
+
+                </td>
+
+              </tr>
+            ))
+          }
+
+        </tbody>
+
+      </table>
+
     </div>
   );
 }
