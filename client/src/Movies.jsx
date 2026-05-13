f2677d4135b9f8c7759b4108d042f90ba3e7a4dd
SnehaRao31078 <sneharao31078@gmail.com>
2026-05-13 11:46:58 +0530

    ch

--- client/src/Movies.jsx
+++ client/src/Movies.jsx
@@ -95,7 +95,7 @@ function Shows() {
         <div className="hero">
           <iframe
  className="hero-video"
-  src="https://youtu.be/TkBP_z9rqiA?si=MinnutzrGGP_kgn6"
+  src="https://www.youtube.com/embed/1qH_9vVZtXo?autoplay=1&mute=1&controls=0&loop=1&playlist=1qH_9vVZtXo"
   title="YouTube video player"
   frameborder="0"
   allow="autoplay; encrypted-media"
